86b8a4057c037430ccae3ad59c5ff279b65d98c1
kozomdoci <zihye.choi@gmail.com>
2023-06-09 16:42:27 +0900

    github tester

--- spring_project_tester/src/main/java/com/lgy/spring_project_tester/dto/MemDto.java
+++ spring_project_tester/src/main/java/com/lgy/spring_project_tester/dto/MemDto.java
@@ -1,12 +1,12 @@
 package com.lgy.spring_project_tester.dto;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
-public class MemDto {
+public class MemDto { // 깃허브테스트
 	private String mem_uid;
 	private String mem_pwd;
 	private String mem_name;
